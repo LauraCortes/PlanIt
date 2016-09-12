946c50aa4b595239b6c6547c40899a623699328b
bdrangel10 <bd.rangel10@uniandes.edu.co>
2016-09-12 13:53:39 -0500

    Cambio de fuente

--- app/src/main/java/com/example/laura/planit/MainActivity.java
+++ app/src/main/java/com/example/laura/planit/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.laura.planit;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
     }
 }
